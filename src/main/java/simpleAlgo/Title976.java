50071058717add343b4b1067bf1a2484ef46a838
Yanping <liuyanping9418@gmail.com>
2020-07-16 16:27:51 +0800

    [Yanping] #976 三角形的最大周长

---
+++ src/main/java/simpleAlgo/Title976.java
@@ -0,0 +1,23 @@
+package simpleAlgo;
+
+import java.util.Arrays;
+
+/**
+ * 三角形的最大周长
+ * 给定由一些正数（代表长度）组成的数组 A，返回由其中三个长度组成的、面积不为零的三角形的最大周长。
+ * 如果不能形成任何面积不为零的三角形，返回 0。
+ */
+public class Title976 {
+    public int largestPerimeter(int[] A) {
+        if (A == null || A.length < 3) {
+            return 0;
+        }
+        Arrays.sort(A);
+        for (int i = A.length - 1; i >= 2  ; i--) {
+            if (A[i] < (A[i - 1] + A[i - 2])) {
+                return (A[i] + A[i - 1] + A[i - 2]);
+            }
+        }
+        return 0;
+    }
+}
