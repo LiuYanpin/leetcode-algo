f3692d536b365c9a661b9ea01984c7d472714a7c
Yanping <liuyanping9418@gmail.com>
2020-08-07 12:53:58 +0800

    [Yanping] 剑指 Offer 28. 对称的二叉树

---
+++ src/main/java/PointToOffer/Offer28.java
@@ -0,0 +1,25 @@
+package PointToOffer;
+
+import common.TreeNode;
+
+/**
+ * 剑指 Offer 28. 对称的二叉树
+ */
+public class Offer28 {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isMirror(root.left, root.right);
+    }
+
+    private boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null || left.val != right.val) {
+            return false;
+        }
+        return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    }
+}
